e0f9c2d0dfc9c93d187eb4ef7340f7771d9fe426
tsMukesh51 <91588183+tsMukesh51@users.noreply.github.com>
2025-08-17 16:32:49 +0530

    cf works

---
+++ honoCldflr/prisma/migrations/20250816113010_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "public"."Todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
--- honoCldflr/src/index.ts
+++ honoCldflr/src/index.ts
@@ -1,9 +1,48 @@
 import { Hono } from 'hono'
+// import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from './generated/prisma/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+app.get('/user', async (c) => {
+  const prismaClient = new PrismaClient({
+    // @ts-ignore
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate());
+  const randomNum = Math.round(10 * Math.random());
+  const user = await prismaClient.user.findFirst({
+    where: {
+      id: randomNum
+    }
+  });
+  return c.json({
+    message: 'User fetched',
+    data: user,
+  });
+});
+
+app.post('/user', async (c) => {
+  const body = await c.req.json();
+  const prismaClient = new PrismaClient({
+    // @ts-ignore
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate());
+  const user = await prismaClient.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password
+    }
+  });
+  return c.json({
+    message: 'User Created',
+    data: user
+  });
+});
+
+export default app;
